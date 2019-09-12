5b9bde91923e71e63221cc8740a13785cc04e2a2
Mateusz Rzeczyca <mateusz_rz@poczta.fm>
2019-09-12 21:58:59 +0200

    docs and license

---
+++ README.md
@@ -0,0 +1,34 @@
+# BMICalculator
+
+BMICalculator is a desktop application, which calculates BMI from given height(cm) and weight(kg).
+
+## Installation
+
+Use the [qmake](https://doc.qt.io/qt-5/qmake-manual.html) to install BMICalculator.
+
+```bash
+qmake BMICalculator.pro
+make
+```
+
+## Usage
+
+```bash
+./BMICalculator
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+
+## Author
+
+Mateusz Rzeczyca
+
+mateuszrzeczyca17@gmail.com
+
+## Screenshots
+
+![alt-text-1](img/default.png "img/default.png") 
+![alt-text-2](img/example_calc.png "img/example_calc.png") 
+![alt-text-3](img/error_height.png "img/error_height.png") 
+![alt-text-4](img/error_not_number.png "img/error_not_number.png")
--- bmicalc.h
+++ bmicalc.h
@@ -7,6 +7,9 @@ namespace Ui {
 class BMICalc;
 }
 
+/**
+* Implements class for operating on Qt-based application
+*/
 class BMICalc : public QMainWindow
 {
     Q_OBJECT
@@ -15,25 +18,39 @@ public:
     explicit BMICalc(QWidget *parent = 0);
     ~BMICalc();
 
-
-    // QWidgets functions
 private slots:
+    /**
+    * Decreases height by clicking the minus button
+    */
     void on_minusHButton_clicked();
 
+    /**
+    * Increases height by clicking the plus button
+    */
     void on_plusHButton_clicked();
 
+    /**
+    * Calculating BMI from given data
+    */
     void on_calcButton_clicked();
 
+    /**
+    * Resets the whole data to standard 
+    */
     void on_resetButton_clicked();
 
+    /**
+    * Decreases weight by clicking the minus button
+    */
     void on_minusWButton_clicked();
 
+    /**
+    * Decreases weight by clicking the plus button
+    */
     void on_plusWButton_clicked();
 
 private:
     Ui::BMICalc *ui;
-
-    // start data
     int height = 100;
     double weight = 50.0;
     std::string heightStr = "100";
@@ -41,60 +58,158 @@ private:
     double result = 0.0;
     std::string resultStr = "0.0";
 
-    // ------
-    // helpful functions
-    // ------
 
-    // shows result of all this
+    /**
+    * Calculates BMI from given data, when there is no error
+    * converts to QString and returns
+    * 
+    * @return bmi
+    */
     QString getResult();
 
-    // resets data on text labels
+    /**
+     * Resets height and weight to standard values
+     */
     void resetData();
 
-    // I use this function only on start of the app to show data on LineEdit Panels
+    /**
+     *  I use this function only on start of the app to show data on LineEdit Panels
+     */
     void showData();
 
-    // gets weight and height, i use this for a reason if user used keyboard to update data
+    /**
+     * Gets height and weight from label. The reason for this method is to get
+     * data, if user decided to use keyboard
+     */ 
     void getData();
 
-    // these I use to update data when a button "plus" or "minus" is clicked
+    /**
+     * Update Height on labels to be actual from clicked button
+     */
     void updateDataFromHeightClick();
+
+    /**
+     * Update Weight on labels to be actual from clicked button
+     */
     void updateDataFromWeightClick();
 
-    // gets and convert height and weight from QString
+    /**
+     * Get height from label in QString format and return in std::string
+     */
     std::string getHeight();
+
+    /**
+     * Get weight from label in QString format and return in std::string
+     */
     std::string getWeight();
 
-    // these functions decrements / increments arguments
-    // height
-    void minus(int&);
-    void plus(int&);
-    // weight
-    void minus(double&);
-    void plus(double&);
+    /**
+     * Decrements int value, int--
+     * 
+     * @param data value to decrement
+     */
+    void minus(int& data);
 
-    // int to string, string to double etc.
-    std::string DbStr(double);
-    std::string IntStr(int);
-    int StrInt(std::string);
-    double StrDb(std::string);
-    std::string QtStr(QString);
-    QString StrQt(std::string);
+    /**
+     * Increments int value, int++
+     * 
+     * @param data value to increment
+     */
+    void plus(int& data);
+    
+    /**
+     * Decrements double value, double--
+     * 
+     * @param data value to decrement
+     */
+    void minus(double& data);
 
-    // looking for some exceptions
-    // checks if provided number by user is a double variable
-    bool onlyNumbersDb(std::string);
-    // the same but with the int variable instead
-    bool onlyNumbersInt(std::string);
+    /**
+     * Incremets double value, double++
+     * 
+     * @param data value to increment
+     */
+    void plus(double& data);
 
-    // updates the result by leaving the number only with 5 characters
-    QString eraseResult(std::string);
+    /**
+     * Converts from double to std::string
+     * 
+     * @param data number in double format to convert
+     */
+    std::string DbStr(double data);
 
-    // issue when height is equal to 0
+    /**
+     * Converts from int to std::string
+     * 
+     * @param data number in int format to convert
+     */
+    std::string IntStr(int data);
+
+    /**
+     * Converts from std::string to in
+     * 
+     * @param data number in std::string to convert
+     */
+    int StrInt(std::string data);
+
+    /**
+     * Converts from std::string to double
+     * 
+     * @param data number in std::string to convert
+     */
+    double StrDb(std::string data);
+
+    /**
+     * Converts from QString to std::string
+     * 
+     * @param data number in QString to convert
+     */
+    std::string QtStr(QString data);
+
+    /**
+     * Converts from std::string to QString
+     * 
+     * @param data number in std::string to convert
+     */
+    QString StrQt(std::string data);
+
+    /**
+     * Checks if std::string is a double number
+     * 
+     * @param data std:string which will be checked
+     */
+    bool onlyNumbersDb(const std::string& data);
+
+    /**
+     * Checks if std::string is a int number
+     * 
+     * @param data std:string which will be checked
+     */
+    bool onlyNumbersInt(const std::string& data);
+
+    /**
+     * Updates the result by leaving the number only with 5 characters
+     *
+     * @param data result, which will be cut
+     */ 
+    QString eraseResult(std::string data);
+
+    /**
+     *  Issue when height is equal to 0
+     */
     void issueWhenHeightIsNull();
 
-    // issue when user doesn't type numbers
+    /** 
+     * Issue when user doesn't type numbers
+     */
     void noNumbersIssue();
+
+    /**
+     *  issue when user uses comma instead of dot
+     * 
+     * @param data std:string, which will be checked
+     */
+    std::string noDotIssue(std::string& data);
 };
 
 #endif // BMICALC_H
