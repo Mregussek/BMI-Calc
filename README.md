0563a40e318ecdc37c56d527d24387c0252172e1
Mateusz Rzeczyca <mateusz_rz@poczta.fm>
2019-09-12 21:58:59 +0200

    docs and license

---
+++ README.md
@@ -0,0 +1,27 @@
+# BMICalculator
+
+BMICalculator is a desktop application, which calculates BMI from given height(cm) and weight(kg).
+
+## Installation
+
+Use the [qmake](https://doc.qt.io/qt-5/qmake-manual.html) to install BMICalculator.
+
+```bash
+qmake BMICalculator.pro
+make
+```
+
+## Usage
+
+```bash
+./BMICalculator
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+
+## Author
+
+Mateusz Rzeczyca
+
+mateuszrzeczyca17@gmail.com
--- bmicalc.cpp
+++ bmicalc.cpp
@@ -1,7 +1,8 @@
 #include "bmicalc.h"
 #include "ui_bmicalc.h"
+#include <iostream> // std::noskipws
 #include <string>
-#include <sstream>
+#include <sstream> // std::istringstream
 #include <qstring.h>
 #include <QMessageBox>
 
@@ -78,9 +79,9 @@ QString BMICalc::getResult()
     result *= 10000;
 
     resultStr = DbStr(result);
-    QString secondHelper = eraseResult(resultStr);
+    QString bmi = eraseResult(resultStr);
 
-    return secondHelper;
+    return bmi;
 }
 
 void BMICalc::resetData()
@@ -130,15 +131,14 @@ void BMICalc::updateDataFromWeightClick()
 std::string BMICalc::getHeight()
 {
     QString helper = ui ->textH ->text();
-    std::string secondHelper = QtStr(helper);
-    return secondHelper;
+    return QtStr(helper);
 }
 
 std::string BMICalc::getWeight()
 {
     QString helper = ui ->textW ->text();
-    std::string secondHelper = QtStr(helper);
-    return secondHelper;
+    std::string secondHelper =  QtStr(helper);
+    return noDotIssue(secondHelper);
 }
 
 void BMICalc::minus(int& data)
@@ -197,53 +197,40 @@ double BMICalc::StrDb(std::string data)
 
 QString BMICalc::StrQt(std::string data)
 {
-    QString helper = QString::fromStdString(data);
-    return helper;
+    return QString::fromStdString(data);
 }
 
 std::string BMICalc::QtStr(QString data)
 {
-    std::string helper = data.toStdString();
-    return helper;
+    return data.toStdString();
 }
 
-bool BMICalc::onlyNumbersDb(std::string data)
+bool BMICalc::onlyNumbersDb(const std::string& data)
 {
-    for(int i = 0; i < data.size(); i++)
-        if(!(data[i] >= 48 && data[i] <= 56))
-        {
-            if(data[i] == 46 || data[i] == 44)
-                continue;
-            else
-                return false;
-        }
-
-    return true;
+    std::istringstream iss(data);
+    float f;
+    iss >> std::noskipws >> f;
+    return iss.eof() && !iss.fail();
 }
 
-bool BMICalc::onlyNumbersInt(std::string data)
+bool BMICalc::onlyNumbersInt(const std::string& data)
 {
-    for(int i = 0; i < data.length(); i++)
-        if(!(data[i] >= 48 && data[i] <= 56))
-            return false;
-
-    return true;
+    return !data.empty() && std::find_if(data.begin(), 
+        data.end(), [](char c) { return !std::isdigit(c); }) == data.end();
 }
 
 QString BMICalc::eraseResult(std::string data)
 {
     data.erase(5, data.length() - 1);
-    QString helper = StrQt(data);
-
-    return helper;
+    return StrQt(data);
 }
 
 void BMICalc::issueWhenHeightIsNull()
 {
     QMessageBox warning;
 
-    warning.setText("Uwaga!");
-    warning.setInformativeText("Wysokość nie może być równa 0!");
+    warning.setText("Warning!");
+    warning.setInformativeText("Height cannot be equal to 0!");
     warning.setStandardButtons(QMessageBox::Ok);
     warning.setDefaultButton(QMessageBox::Ok);
     int run = warning.exec();
@@ -253,9 +240,20 @@ void BMICalc::noNumbersIssue()
 {
     QMessageBox warning;
 
-    warning.setText("Uwaga!");
-    warning.setInformativeText("Dane muszą być liczbami!");
+    warning.setText("Warning!");
+    warning.setInformativeText("Given data must be a number!\nNote:\nUse dot (72.0), not comma!");
     warning.setStandardButtons(QMessageBox::Ok);
     warning.setDefaultButton(QMessageBox::Ok);
     int run = warning.exec();
+}
+
+std::string BMICalc::noDotIssue(std::string& data)
+{
+    for(int i = 0; i < data.length(); i++)
+    {
+        if(data[i] == ',')
+            data[i] = '.';
+    }
+
+    return data;
 }
